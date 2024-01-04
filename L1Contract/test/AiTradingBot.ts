548ac22bbd74c4a8551329e118a4aa3e81506846
Tomoya Kobayashi <tomocha.marika@gmail.com>
2024-01-04 12:21:02 +0900

    rough test

--- L1Contract/test/AiTradingBot.ts
+++ L1Contract/test/AiTradingBot.ts
@@ -1,11 +1,11 @@
 import { describe } from "node:test";
-import { ethers } from "hardhat-ethers";
+import { ethers } from "hardhat";
 
 describe("AI Trading Bot", () => {
   it("Should be able to deploy the contract", async () => {
     const AiTradingBot = await ethers.getContractFactory("AiTradingBot");
     const aiTradingBot = await AiTradingBot.deploy();
-    await aiTradingBot.deployed();
+    // await aiTradingBot.deployed();
     console.log("AiTradingBot deployed to:", aiTradingBot.address);
   });
 });
