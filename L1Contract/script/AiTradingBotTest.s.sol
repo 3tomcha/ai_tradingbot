7f0cd945c0be95f45007b20ee8836b9225fefa37
Tomoya Kobayashi <tomocha.marika@gmail.com>
2024-01-13 10:34:42 +0900

    rename

--- L1Contract/script/AiTradingBotTest.s.sol
+++ L1Contract/script/AiTradingBotTest.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Script, console2} from "forge-std/Script.sol";
 
-contract AiScript is Script {
+contract AiTradingBotTest is Script {
     function setUp() public {}
 
     function run() public {
